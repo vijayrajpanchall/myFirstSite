aa34fa78905e78896e264c650209709a444fefb7
vijay-pedalsup <vijay.panchal@pedalsup.com>
2023-06-15 18:56:09 +0530

    created server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('project started')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    
+});
+
+app.listen(3000, () => {
+    console.log("server started");
+});
