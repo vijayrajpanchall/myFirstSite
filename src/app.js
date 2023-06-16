913fbf757ca789f54d33106874c69c96c0c29adf
vijay-pedalsup <vijay.panchal@pedalsup.com>
2023-06-16 13:12:51 +0530

    connect with db, img, navbar

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const routes = require('./routes/main');
 const hbs = require('hbs');
 
 app.use('', routes);
+app.use('/static',express.static('public'));
 
 app.set('view engine', 'hbs');
 app.set("views","views")
+hbs.registerPartials('views/partials');
+
+//db connection
+mongoose.connect('mongodb://localhost/website_tut', () => {
+    console.log("db connected");
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
