cd14e51d8a2fc23115e5913ac65ed9243d67e196
vijay-pedalsup <vijay.panchal@pedalsup.com>
2023-06-16 16:33:01 +0530

    db connectivity and fetching from db

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const routes = require('./routes/main');
 const hbs = require('hbs');
-
+const Detail = require('./models/Detail');
 app.use('', routes);
 app.use('/static',express.static('public'));
 
@@ -14,6 +14,33 @@ hbs.registerPartials('views/partials');
 //db connection
 mongoose.connect('mongodb://localhost/website_tut', () => {
     console.log("db connected");
+    // Detail.create({
+    //     brandName: "Info Technical Solutions",
+    //     brandIconUrl: "https://www.infotechnicalsolutions.com/wp-content/uploads/2019/01/Info-Technical-Solutions-Logo.png",
+    //     links: [{
+    //         label: "Home",
+    //         url: "/"
+    //     },
+    //     {
+    //         label: "Services",
+    //         url: "/services"
+    //     },
+    //     {
+    //         label: "About",
+    //         url: "/about"
+    //     },
+    //     {
+    //         label: "Contact",
+    //         url: "/contact"
+    //     }]
+    // }, (err, detail) => {
+    //     if(err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log(detail);
+    //     }
+    // }
+    // );
 });
 
 app.get('/', (req, res) => {
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    res.render("index");
+const Detail = require('../models/Detail');
+
+routes.get('/', async (req, res) => {
+    const details = await Detail.findOne({ _id: "648c2f8e76b7599ef40789e2"});
+    console.log(details);
+    res.render("index", {
+        details:details
+    });
 });
 
 routes.get('/gallary', (req, res) => {
