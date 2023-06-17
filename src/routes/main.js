6710662fad50e29fa672c32ac328093b5ea9bfc4
vijay-pedalsup <vijay.panchal@pedalsup.com>
2023-06-17 12:27:24 +0530

    getting dynamic data from db for navbar

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,14 +5,16 @@ const Detail = require('../models/Detail');
 
 routes.get('/', async (req, res) => {
     const details = await Detail.findOne({ _id: "648c2f8e76b7599ef40789e2"});
-    console.log(details);
     res.render("index", {
         details:details
     });
 });
 
-routes.get('/gallary', (req, res) => {
-    res.render("gallary");
+routes.get('/gallary', async (req, res) => {
+    const details = await Detail.findOne({ _id: "648c2f8e76b7599ef40789e2" });
+    res.render("gallary", {
+        details: details
+    });
 });
 
 module.exports = routes;
